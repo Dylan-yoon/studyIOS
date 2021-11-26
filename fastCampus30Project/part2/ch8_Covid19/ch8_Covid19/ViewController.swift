d091c556feceee05f64e0c2b089be1e23b14517c
Dylan-yoon <d.dylany21@gmail.com>
2021-11-27 02:23:52 +0900

    clear p2c8

--- fastCampus30Project/part2/ch8_Covid19/ch8_Covid19/ViewController.swift
+++ fastCampus30Project/part2/ch8_Covid19/ch8_Covid19/ViewController.swift
@@ -13,18 +13,27 @@ import Charts       //파이차트 사용하기위해 인포트한다
 class ViewController: UIViewController {
 
     @IBOutlet weak var totalCaseLabel: UILabel!
-    
     @IBOutlet weak var newCaseLabel: UILabel!
     
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var labelStackView: UIStackView!
     @IBOutlet weak var pieChartView: PieChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.indicatorView.startAnimating()
         self.fetchCovidOverview(completionHandler: { [weak self] result in
             guard let self = self else { return }
+            self.indicatorView.stopAnimating()
+            self.indicatorView.isHidden = true
+            self.labelStackView.isHidden = false
+            self.pieChartView.isHidden = false
+            
             switch result {
             case let .success(result):
                 self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(CityCovidOverview: result)
+                self.configureChartView(covidOverviewList: covidOverviewList)
                 
             case let .failure(error) :
                 debugPrint("error \(error)")
@@ -58,12 +67,32 @@ class ViewController: UIViewController {
     }
     
     func configureChartView(covidOverviewList : [CovidOverview]) {
-        let entries = covidOverviewList.compactMap{ }[weak self] overview -> PieChartDataEntry? in
-        guard let self = self else { return nil }
-        return PieChartDataEntry(
-            value: self.removeFormatString(string: overview.newCase),
-            label: overview.countryName,
-            data: overview)
+        self.pieChartView.delegate = self
+        let entries = covidOverviewList.compactMap{ [weak self] overview -> PieChartDataEntry? in
+            guard let self = self else { return nil }
+            return PieChartDataEntry(
+                value: self.removeFormatString(string: overview.newCase),
+                label: overview.countryName,
+                data: overview
+            )
+        }
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        dataSet.sliceSpace = 1 //데이터 항목간 간격
+        dataSet.entryLabelColor = .black
+        dataSet.valueTextColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueLinePart1OffsetPercentage = 0.8
+        dataSet.valueLinePart1Length = 0.2
+        dataSet.valueLinePart2Length = 0.3
+        
+        dataSet.colors =  ChartColorTemplates.vordiplom() +
+        ChartColorTemplates.joyful() +
+        ChartColorTemplates.pastel() +
+        ChartColorTemplates.liberty() +
+        ChartColorTemplates.material()
+        
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+        self.pieChartView.spin(duration: 0.3, fromAngle: self.pieChartView.rotationAngle, toAngle: self.pieChartView.rotationAngle + 80)
     }
     
     func removeFormatString(string : String) -> Double {
@@ -104,3 +133,13 @@ class ViewController: UIViewController {
     }
 }
 
+
+
+extension ViewController: ChartViewDelegate {
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        guard let covidDetailVoewController = self.storyboard?.instantiateViewController(identifier: "CovidDetailViewController") as? CovidDetailViewController else { return }
+        guard let covidOverview = entry.data as? CovidOverview else { return }
+        covidDetailVoewController.covidOverview = covidOverview
+        self.navigationController?.pushViewController(covidDetailVoewController, animated: true)
+    }
+}
