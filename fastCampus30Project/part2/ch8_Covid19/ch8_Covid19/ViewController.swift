f13f5a6d2507e3ebfb786359c9f73d0935b1e7f7
Dylan-yoon <d.dylany21@gmail.com>
2021-11-26 16:33:19 +0900

    p2c8 covid ,today study

--- fastCampus30Project/part2/ch8_Covid19/ch8_Covid19/ViewController.swift
+++ fastCampus30Project/part2/ch8_Covid19/ch8_Covid19/ViewController.swift
@@ -13,7 +13,8 @@ import Charts       //파이차트 사용하기위해 인포트한다
 class ViewController: UIViewController {
 
     @IBOutlet weak var totalCaseLabel: UILabel!
-    @IBOutlet weak var newCaseLabel: UIStackView!
+    
+    @IBOutlet weak var newCaseLabel: UILabel!
     
     @IBOutlet weak var pieChartView: PieChartView!
     
@@ -23,12 +24,58 @@ class ViewController: UIViewController {
             guard let self = self else { return }
             switch result {
             case let .success(result):
-                debugPrint("success \(result)")
+                self.configureStackView(koreaCovidOverview: result.korea)
                 
             case let .failure(error) :
                 debugPrint("error \(error)")
             }
         })
+    }
+    
+    //파이차트에 표시하는 메서드
+    func makeCovidOverviewList(
+    CityCovidOverview: CityCovidOverview
+    ) -> [CovidOverview] {
+     return [
+        CityCovidOverview.seoul,
+        CityCovidOverview.busan,
+        CityCovidOverview.daegu,
+        CityCovidOverview.incheon,
+        CityCovidOverview.gwangju,
+        CityCovidOverview.daejeon,
+        CityCovidOverview.ulsan,
+        CityCovidOverview.sejong,
+        CityCovidOverview.gyeonggi,
+        CityCovidOverview.gangwon,
+        CityCovidOverview.chungbuk,
+        CityCovidOverview.chungnam,
+        CityCovidOverview.jeonbuk,
+        CityCovidOverview.jeonnam,
+        CityCovidOverview.gyeongbuk,
+        CityCovidOverview.gyeongnam,
+        CityCovidOverview.jeju,
+     ]
+    }
+    
+    func configureChartView(covidOverviewList : [CovidOverview]) {
+        let entries = covidOverviewList.compactMap{ }[weak self] overview -> PieChartDataEntry? in
+        guard let self = self else { return nil }
+        return PieChartDataEntry(
+            value: self.removeFormatString(string: overview.newCase),
+            label: overview.countryName,
+            data: overview)
+    }
+    
+    func removeFormatString(string : String) -> Double {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: string)?.doubleValue ?? 0
+    }
+    
+    
+    func configureStackView(koreaCovidOverview: CovidOverview) {
+        self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.newCase)명"
     }
     
     func fetchCovidOverview(
