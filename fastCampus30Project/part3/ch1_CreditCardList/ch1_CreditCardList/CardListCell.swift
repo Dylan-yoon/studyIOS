a44323cf24aa76515a502828122f407dbbef03ea
Dylan-yoon <d.dylany21@gmail.com>
2022-01-05 20:37:18 +0900

    part3 chapter2 creditCard _ starting , UI setting , Json .etc

---
+++ fastCampus30Project/part3/ch1_CreditCardList/ch1_CreditCardList/CardListCell.swift
@@ -0,0 +1,27 @@
+//
+//  CardListCell.swift
+//  ch1_CreditCardList
+//
+//  Created by 윤병희 on 2022/01/05.
+//
+
+import UIKit
+
+class CardListCell: UITableViewCell {
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var promotionLabel: UILabel!
+    @IBOutlet weak var cardNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
