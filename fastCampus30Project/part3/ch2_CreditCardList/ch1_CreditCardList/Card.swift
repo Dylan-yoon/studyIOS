a75eb683af6b7107b01b499db8878a0b1f6619a1
Dylan-yoon <d.dylany21@gmail.com>
2022-01-06 02:00:35 +0900

    part3 chapter2,UI configuration is complete

---
+++ fastCampus30Project/part3/ch2_CreditCardList/ch1_CreditCardList/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  ch2_CreditCardList
+//
+//  Created by 윤병희 on 2022/01/06.
+//
+
+import Foundation
---
+++ fastCampus30Project/part3/ch2_CreditCardList/ch1_CreditCardList/CardListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  CardListViewController.swift
+//  ch1_CreditCardList
+//
+//  Created by 윤병희 on 2022/01/05.
+//
+
+import UIKit
+import Kingfisher
+
+class CardListViewController : UITableViewController {
+    var creditCardList : [CreditCard] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //UITableView Cell Register
+        let nibName = UINib(nibName: "CardListCell", bundle: nil)
+        tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return creditCardList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CardListCell", for: indexPath) as? CardListCell else { return UITableViewCell() }
+        
+        cell.rankLabel.text = "\(creditCardList[indexPath.row].rank)"
+        cell.promotionLabel.text = "\(creditCardList[indexPath.row].promotionDetail.amount)만원 증정"
+        cell.cardNameLabel.text = "\(creditCardList[indexPath.row].name)"
+        
+        let imageURL = URL(string: creditCardList[indexPath.row].cardImageURL)
+        cell.cardImageView.kf.setImage(with: imageURL)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //상세 화면 전달
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDetailViewController") as? CardDetailViewController else { return }
+        
+        detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
+        self.show(detailViewController, sender: nil)
+                
+    }
+}
+
+
+//이미지를 모든 기기에 저장해서 쓰는것이 아닌,,, 이미지URL 만 전달받은 상태에서 이미지뷰에 표현,,, 이 오픈소스를 킹피셔 KingFisher
