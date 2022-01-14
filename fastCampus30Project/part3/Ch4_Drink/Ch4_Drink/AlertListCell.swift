4bf857d4c32396b084f767ed5cce0755f116af48
Dylan-yoon <d.dylany21@gmail.com>
2022-01-15 01:16:04 +0900

    part3 chap4 Drinkwater upload

--- fastCampus30Project/part3/Ch4_Drink/Ch4_Drink/AlertListCell.swift
+++ fastCampus30Project/part3/Ch4_Drink/Ch4_Drink/AlertListCell.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import UserNotifications
 
 class AlertListCell: UITableViewCell {
 
+    let userNotificationCenter = UNUserNotificationCenter.current()
+    
     @IBOutlet weak var meridiemLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var alertSwitch: UISwitch!
@@ -25,12 +28,18 @@ class AlertListCell: UITableViewCell {
     
     @IBAction func alertSwitchValueChange(_ sender: UISwitch) {
         
-        //??강의 중간에 없던거같은데? ㅋㅋㅋ
+        //??강의 중간에 없던거같은데?
         
         guard let data = UserDefaults.standard.value(forKey: "alerts") as? Data,
               var alerts = try? PropertyListDecoder().decode([Alert].self, from: data) else { return }
         
         alerts[sender.tag].isOn = sender.isOn
         UserDefaults.standard.set(try? PropertyListEncoder().encode(alerts), forKey: "alerts")
+        
+        if sender.isOn {
+            userNotificationCenter.addNotificationRequest(by : alerts[sender.tag])
+        } else {
+            userNotificationCenter.removePendingNotificationRequests(withIdentifiers: [alerts[sender.tag].id])
+        }
     }
 }
