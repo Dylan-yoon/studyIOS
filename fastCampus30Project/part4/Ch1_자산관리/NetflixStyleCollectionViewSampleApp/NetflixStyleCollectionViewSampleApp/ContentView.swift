c792306e3984ae8e0f9ac15cb069e8f2998037d6
Dylan-yoon <d.dylany21@gmail.com>
2022-02-03 01:40:49 +0900

    part4 chapter1 _ 3 SwiftUI 2

---
+++ fastCampus30Project/part4/Ch1_자산관리/NetflixStyleCollectionViewSampleApp/NetflixStyleCollectionViewSampleApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  NetflixStyleCollectionViewSampleApp
+//
+//  Created by 윤병희 on 2022/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let titles = ["Netflix Sample App"]
+    var body: some View {
+        NavigationView {
+            List(titles, id: \.self) {
+            let netflixVC = HomeViewControllerRepresentable()
+                .navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.all)
+                NavigationLink($0, destination: netflixVC)
+            }
+            .navigationTitle("SwiftUI to UIKit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- fastCampus30Project/part4/Ch1_자산관리/NetflixStyleCollectionViewSampleApp/NetflixStyleCollectionViewSampleApp/HomeViewController.swift
+++ fastCampus30Project/part4/Ch1_자산관리/NetflixStyleCollectionViewSampleApp/NetflixStyleCollectionViewSampleApp/HomeViewController.swift
@@ -191,24 +191,29 @@ extension HomeViewController {
     
     //셀 선택
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let sectionName = contents[indexPath.section].sectionName
-        print("TEST: \(sectionName) 섹션의 \(indexPath.row + 1)번째 콘텐츠")
+        let isFirstSection = indexPath.section == 0
+        let seletedItem = isFirstSection ? mainItem : contents[indexPath.section].contentItem[indexPath.row]
+        
+        let contentDetailView = ContentDetailView(item: seletedItem)
+        let hostingVC = UIHostingController(rootView: contentDetailView)
+        self.show(hostingVC, sender: nil)
     }
 }
 
 //SwiftUI를 활용한 미리보기
 struct HomeViewController_Previews: PreviewProvider {
     static var previews: some View {
-        Container().edgesIgnoringSafeArea(.all)
+        HomeViewControllerRepresentable().edgesIgnoringSafeArea(.all)
     }
     
-    struct Container: UIViewControllerRepresentable {
-        func makeUIViewController(context: Context) -> UIViewController {
-            let layout = UICollectionViewFlowLayout()
-            let homeViewController = HomeViewController(collectionViewLayout: layout)
-            return UINavigationController(rootViewController: homeViewController)
-        }
-        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
-        typealias UIViewControllerType = UIViewController
+}
+
+struct HomeViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let layout = UICollectionViewFlowLayout()
+        let homeViewController = HomeViewController(collectionViewLayout: layout)
+        return UINavigationController(rootViewController: homeViewController)
     }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    typealias UIViewControllerType = UIViewController
 }
