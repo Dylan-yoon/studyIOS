129633af9676be47ad534558940b34a111efed39
Dylan-yoon <d.dylany21@gmail.com>
2022-02-06 14:27:10 +0900

    part4 chapter1 7

---
+++ fastCampus30Project/part4/Ch1_자산관리/MyAssets/MyAssets/AssetSummaryView/AssetSectionView.swift
@@ -0,0 +1,48 @@
+//
+//  AssetSectionView.swift
+//  MyAssets
+//
+//  Created by 윤병희 on 2022/02/06.
+//
+
+import SwiftUI
+
+struct AssetSectionView: View {
+    @ObservedObject var assetSection: Asset
+    var body: some View {
+        VStack(spacing: 20) {
+            AssetSectionHeaderView(title: assetSection.type.title)
+            ForEach(assetSection.data) { asset in
+                HStack {
+                    Text(asset.title)
+                        .font(.title)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                    Text(asset.amount)
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                }
+                Divider()
+            }
+        }
+        .padding()
+    }
+}
+
+
+struct AssetSectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        let asset = Asset(
+            id: 0,
+            type : .bankAccount,
+            data: [
+            AssetData(id: 0, title: "신한은행", amount: "13,300,000원"),
+            AssetData(id: 1, title: "국민은행", amount: "53,300,000원"),
+            AssetData(id: 2, title: "카카오뱅크", amount: "5300,000원")
+            ]
+            
+        )
+        
+        AssetSectionView(assetSection: asset)
+    }
+}
